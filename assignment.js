032db65891946eb2110bcc321ef755e9361986b0
unknown <sayedhossasishifat706@gmail.com>
2021-07-31 06:51:54 +0600

    some change will be done

---
+++ assignment.js
@@ -0,0 +1,64 @@
+        //kilometerToMeter start
+        function kilometerToMeter(kilometer){
+
+            let meter = kilometer*1000;
+            return meter
+        }
+        const result = kilometerToMeter(5);
+        console.log(result)
+        //kilometerToMeter end
+
+
+        //bugetCalculator start
+        function bugetCalculator(watch,phone,laptop){
+            var watch = watch*3;
+            var phone = phone*3;
+            var laptop = laptop*3;
+            var grandTotal = watch + phone + laptop;
+            return grandTotal
+        }
+        var output = bugetCalculator(50,100,500); 
+        console.log(output)
+        //bugetCalculator end
+
+
+        //hotelCost start
+        function hotelCost(stayInHotel){
+        var hotelRent = 0;
+        if(stayInHotel<=10){
+            hotelRent = stayInHotel*100;
+        }
+        else if(stayInHotel<=20){
+            var firstTenDays = 10*100;
+            var remainingDays = stayInHotel - 10;
+            var secondTenDays = remainingDays*80;
+            hotelRent = firstTenDays + secondTenDays;
+        }
+        else{
+            var firstTenDays = 10*100;
+            var secondTenDays = 10*80;
+            var remainingDays = stayInHotel - 20;
+            var thirdTenDays = remainingDays*50;
+            hotelRent = firstTenDays + secondTenDays + thirdTenDays;
+        }
+        return hotelRent
+        }
+        var output = hotelCost(30);
+        console.log(output)
+        //hotelCost end
+
+        //megaName start
+        function megaFriend(friends){
+        var megaName = '';
+            for (let i = 0; i < friends.length; i++) {
+                const friend = friends[i];
+                if(friend.length>megaName.length){
+                    megaName = friend
+                }
+            }
+            return megaName;
+        }
+            var friends = ['Messi','Neymar', 'Ronaldo-CR-7','Ronaldinho','David Beckham' ,'Sakib Al Hasan'];
+            var output = megaFriend(friends);
+            console.log(output)
+            //megaName end
